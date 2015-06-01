99eaaa8b0dc8a5519b1df633d7126d803e8dcc9e
Rasesh Patel <Rasesh@shoptiques.com>
2015-06-01 00:02:57 -0400

    Vanity refactoring

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,27 +4,16 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def remember(user)
-    user.remember
-    cookies.signed.permanent[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_digest
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
   end
 
-
-
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        # log_in user will typically only run once.  If after they've been verifie4d
-        # the user id is stored in session so reunning current user will get caught
-        # in first user lookup user find by session id.
-        log_in user
-        @current_user = user
-      end
-    end
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def forget(user)
@@ -33,10 +22,19 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
-  def log_out
-    forget(current_user)
-    session.delete(:user_id)
-    @current_user = nil
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        # log_in user will typically only run once.  If after they've been verifie4d
+        # the user id is stored in session so reunning current user will get caught
+        # in first user lookup user find by session id.
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
