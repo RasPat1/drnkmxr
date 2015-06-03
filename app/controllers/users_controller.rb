73d3a9803b636c4f05e9b79c9a1d12e590ec9fee
Rasesh Patel <Rasesh@shoptiques.com>
2015-06-03 08:46:55 -0400

    requrie user to be logged in to edit their profile + test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -29,6 +31,14 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'edit'
+    end
+  end
+
+  # Before Filters
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
   end
 
