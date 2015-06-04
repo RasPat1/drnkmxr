39731ed7d7077faf73a9b10bd62ca8c7ab51e8ad
Rasesh Patel <Rasesh@shoptiques.com>
2015-06-03 22:57:13 -0400

    seed some sick nasty fake users bitches

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Example User",
+  email: "example@drnkmxr.com",
+  password: "boss candy",
+  password_confirmation: "boss candy")
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@drnkmxr.com"
+  password = "jkjk!!234"
+  User.create!(name: name,
+    email: email,
+    password: password,
+    password_confirmation: password)
+end
