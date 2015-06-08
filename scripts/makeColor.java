47025ee52149c53f7bc9bdc777a20a44c3a8cb00
Rasesh Patel <Rasesh@shoptiques.com>
2015-06-08 00:13:47 -0400

    add color generation code... for fun

---
+++ scripts/makeColor.java
@@ -0,0 +1,27 @@
+class makeColor {
+
+  public static void main(string) {
+
+
+
+  }
+
+  public Color generateRandomColor(Color mix) {
+      Random random = new Random();
+      int red = random.nextInt(256);
+      int green = random.nextInt(256);
+      int blue = random.nextInt(256);
+
+      // mix the color
+      if (mix != null) {
+          red = (red + mix.getRed()) / 2;
+          green = (green + mix.getGreen()) / 2;
+          blue = (blue + mix.getBlue()) / 2;
+      }
+
+      Color color = new Color(red, green, blue);
+      return color;
+  }
+
+}
+
