a33c8abfbced2b83cb7f4397af3bbf257c03e8c8
Rasesh Patel <Rasesh@shoptiques.com>
2015-07-09 21:10:15 -0400

    book controlelr and associated files with ajax add action

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,8 @@
+class BookController < ApplicationController
+  def ajax_add
+    # save user info
+    # send us an email with that info
+    # return a partial with the thank you message
+    render "ajax_add"
+  end
+end
