f317c48ac34e98d9b26fe4b9460a7bd5b8e1abf3
Rasesh Patel <Rasesh@shoptiques.com>
2015-07-14 22:50:17 -0400

    the db was migrated to includ the booking model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606032228) do
+ActiveRecord::Schema.define(version: 20150714124028) do
+
+  create_table "bookings", force: :cascade do |t|
+    t.string   "email"
+    t.string   "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
