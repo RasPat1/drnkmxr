71a32f9dee2df62682447e3578f6c3178ee8da61
Rasesh Patel <Rasesh@shoptiques.com>
2015-07-14 23:17:08 -0400

    Accept the form and create a booking
    THis needs to use strong parameters rather than just saving whatever came through the params hash

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,16 @@
 class BookingsController < ApplicationController
   def create
+    @booking = Booking.new(:zip => params[:booking][:zip])
+
+    if @booking.save
+      # @booking.send_booking_email
+      # render "success"
+    else
+      # render "fail"
+    end
     # save user info
     # send us an email with that info
     # return a partial with the thank you message
+    render nothing: true
   end
 end
