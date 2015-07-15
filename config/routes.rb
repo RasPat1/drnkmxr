488049e55b0ba28b10c7a02cc1097a8e8e19b2fe
Rasesh Patel <Rasesh@shoptiques.com>
2015-07-14 23:16:32 -0400

    fix booking path so form helper can find it automatically

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'login'         =>  'sessions#new'
   post 'login'        =>  'sessions#create'
   delete 'logout'     =>  'sessions#destroy'
-  post 'book'         =>  'bookings#create'
+  post 'bookings'     =>  'bookings#create'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
