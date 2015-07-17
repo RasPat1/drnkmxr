d4bffec86bfc9ba9cbf30d8e1c975ef27c6266b9
Rasesh Patel <Rasesh@shoptiques.com>
2015-07-17 09:14:12 -0400

    email and zip are not nullable

--- db/migrate/20150714124028_create_bookings.rb
+++ db/migrate/20150714124028_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration
   def change
     create_table :bookings do |t|
-      t.string :email
-      t.string :zip
+      t.string :email null:false
+      t.string :zip null:false
       t.timestamps null: false
     end
   end
