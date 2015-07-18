cd368a3c4c8a92b59ad33100c6f70d841b2d48e6
Rasesh Patel <Rasesh@shoptiques.com>
2015-07-18 04:10:01 -0400

    add validation for emails

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,8 @@
 class Booking < ActiveRecord::Base
   validates :zip, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
   def send_booking_submitted
     UserMailer.booking_submitted(self).deliver_now
   end
