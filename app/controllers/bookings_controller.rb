58e3050c2ba0c55513860ccf31d30a95fd4d8392
Rasesh Patel <Rasesh@shoptiques.com>
2015-07-21 09:26:06 -0400

    return a message after successful and failed saves

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,11 +3,12 @@ class BookingsController < ApplicationController
     @booking = Booking.new(
       :zip => params[:booking][:zip],
       :email => params[:booking][:email])
-    result = {success: false}
+    result = {success: false, message: 'Sorry, try again'}
     if @booking.save # save user info
       # send us an email with that info
       @booking.send_booking_submitted
       result[:success] = true
+      result[:message] = "Thanks for registering with drnk|mxr"
     end
 
     # return a partial with the thank you message
