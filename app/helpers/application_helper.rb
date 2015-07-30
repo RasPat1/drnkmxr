0632cedd2775157bedee316b8df2e7fcf4e6fa61
Rasesh Patel <Rasesh@shoptiques.com>
2015-07-29 23:54:32 -0400

    keep heroku instance from falling asleep

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis
   def full_title(page_title = '')
-    base_title = "Vamos a la playa, ohohohoh"
+    base_title = "Bartenders on Demand"
     if page_title.empty?
       base_title
     else
---
+++ scripts/keepHerokuAwake.sh
@@ -0,0 +1,1 @@
+while [ true ]; do curl -L -v --header "Connection: keep-alive" "http://drnkmxr.herokuapp.com"; sleep 3000; done
