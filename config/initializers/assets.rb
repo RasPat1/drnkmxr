51a4e9abb44fc7065380662867af31dc4e3b18c0
Rasesh Patel <Rasesh@shoptiques.com>
2015-08-03 06:23:24 -0400

    just testing font compilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-require 'flaticon'
+config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 Rails.application.config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
 # Precompile additional assets.
