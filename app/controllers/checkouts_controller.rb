4fa02e371c5f95333998108e2ddbbebbf3c207c3
Rasesh Patel <raspat1@gmail.com>
2020-05-16 21:26:37 -0700

    Committing loose code in this old repository. #SpringCleaning

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -12,6 +12,32 @@ class CheckoutsController < ApplicationController
     end
   end
 
+  def create_menu
+    drinkIds = params["drink-ids"]
+    @drinks = []
+    if (drinkIds)
+      drinkIds.split(',').each do |drinkId|
+        @drinks += [Drink.find(drinkId)]
+      end
+    end
+    if @drinks.size > 0
+      @menu = Menu.new
+      @menu.drinks = @drinks
+      @menu.save
+      if (@menu)
+        redirect_to action: :create_event
+      end
+    end
+
+  end
+
+  def create_event
+
+  end
+
+  def login
+  end
+
   def create
     drinkIds = params["drink-ids"]
     @drinks = []
@@ -25,12 +51,13 @@ class CheckoutsController < ApplicationController
       @menu.drinks = @drinks
       @menu.save
       if (@menu)
-        @event = Event.new
-        @event.menu = @menu
-        @event.save
+        redirect_to new_event_path(:menu_id => @menu)
       end
-      redirect_to edit_event_path(@event)
     end
+
+  end
+
+  def edit
   end
 
 end
